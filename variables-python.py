ffccc25b7e12541432ac173ebae8d6ea280c4b88
tina_m <librariantinamorrell@gmail.com>
2022-02-11 17:22:18 -0700

    variable examples

--- variables-python.py
+++ variables-python.py
@@ -1,14 +1,14 @@
 #three types of variables - string, integer, float
-\
+
 #string - grouping of characters. can be numbers, letters, or symbols. quotes are placed around it.
 greeting = "Hey there, "
 print(greeting + "you brilliant person!")
-\
+
 #integer - whole numbers. don't need quotes around it
 high_temperature = 10
 low_temperature = -6
 print(high_temperature + low_temperature)
-\
+
 #float - numbers that have a decimal
 cat_food_price = 54.99
 tax = .05
