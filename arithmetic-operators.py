0209cb6203c560db83eb7364f901b6186166465c
tina_m <librariantinamorrell@gmail.com>
2022-02-11 21:43:50 -0700

    arithmetic operators practice

---
+++ arithmetic-operators.py
@@ -0,0 +1,24 @@
+#arithmetic operators - addition (+), subtraction (-),multiplication (*), and division (/)
+
+#addition (+)
+print(46.7 + 24.8)
+
+#subtraction (-)
+print(8 - 2)
+
+#multiplication (*)
+print(6 * 2)
+
+#division (/)
+print(12 / 6)
+
+#repetition operator
+food_order = "sesame chicken, "
+print(food_order * 5)
+
+#arithmetic operators between variable names to calculate the value of those variables
+cats = 5
+ferrets = 4
+dog = 1
+pets = cats + ferrets + dog
+print(pets)
