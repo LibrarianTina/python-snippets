b68d5acf494d49f0ddf07b9f9db7ba625b62b5c2
tina_m <librariantinamorrell@gmail.com>
2022-02-19 22:47:37 -0700

    selecting and slicing items in lists

---
+++ lists-selecting-slicing.py
@@ -0,0 +1,22 @@
+female_superheros = ["Wonder Woman", "Elastagirl", "Cat Woman", "Storm", "Black Widow"]
+
+#selecting single item
+female_superheros[2]
+print(female_superheros [2])
+
+#slicing - multiple items in the middle of the list
+female_superheros[1:3]
+print(female_superheros[1:3])
+
+#slicing - making a new variable
+dc_heros = female_superheros[:3]
+print(female_superheros[:3])
+
+#slicing multiple items, starting with 0 and ending with position 1
+female_superheros[:2]
+print(female_superheros[:2])
+
+#slicing mutiple items, starting with position 2 and going to the end of the list
+female_superheros[2:]
+print(female_superheros[2:])
+
