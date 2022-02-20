732c6e79378111b09a65966bc8d9eed371aa7d80
tina_m <librariantinamorrell@gmail.com>
2022-02-19 20:05:36 -0700

    adding and deleting items in lists

---
+++ lists-adding-deleting.py
@@ -0,0 +1,11 @@
+female_superheros = ["Wonder Woman", "Elastagirl", "Cat Woman", "Storm", "Black Widow"]
+#remove last item
+female_superheros.pop()
+#remove third item of list
+female_superheros.pop(2)
+#add item to end of list
+female_superheros.append("Batgirl")
+#add item to middle of list
+female_superheros.insert(3, "Scarlet Witch")
+
+print(female_superheros)
